ae4c14f4753d665b458f9d5d77096430b50537c3
Harshil Sarariya <harshilsarariya@users.noreply.github.com>
2022-12-28 17:35:58 +0530

    ideal server api added

--- src/api/client.js
+++ src/api/client.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-  // baseURL: "https://quickfix-service-app-server.herokuapp.com/api",
-  baseURL: "http://192.168.165.159:5000/api",
+  baseURL: "https://ideal-server.onrender.com/api",
+  // baseURL: "http://192.168.2.102:5000/api",
 });
 
 export default client;
